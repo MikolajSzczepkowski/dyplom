2272061ce3a9c4e676efaf45c9aee1f3e0a51957
Mikolaj Szczepkowski <mikolajszczepkowski@gmail.com>
2016-05-17 12:14:26 +0200

    mobile styling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 $(function(){
-
+  $("#openGallery").on("click", function(){
+    if ($("#gallery").hasClass("active")) {
+      $("#gallery").removeClass("active");
+      $("#gallery").hide("slide", {
+        direction: "right",
+        easing: "easeInQuint"
+      }, 400);
+    }
+    else {
+      $("#gallery").addClass("active");
+      $("#gallery").show("slide", {
+        direction: "right",
+        easing: "easeOutQuint"
+      }, 600);
+    }
+  });
 });
