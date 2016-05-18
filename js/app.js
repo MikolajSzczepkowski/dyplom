03a803e7ffa1c9c4cc3190c8fee65f44fc35f90d
Mikolaj Szczepkowski <mikolajszczepkowski@gmail.com>
2016-05-18 16:09:28 +0200

    modal backgrounds colors

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@ $(function(){
   var image = [
     { title: "A great wave by the coast",
       src: "images/hiroshige/Hiroshige_A_great_wave_by_the_coast.jpg",
-      color: "#7392aa",
-      darkerColor: "#516778",
+      color: "#e3d4bd",
+      darkerColor: "#AB8E6C",
       text: " It is Hokusai's most famous work, and one of the best recognized works of Japanese art in the world. It depicts an enormous wave threatening boats off the coast of the prefecture of Kanagawa. While sometimes assumed to be a tsunami, the wave is, as the picture's title suggests, more likely to be a large rogue wave.[2] As in all the prints in the series, it depicts the area around Mount Fuji under particular conditions, and the mountain itself appears in the background."
     },
     { title: "Evening view of a temple in the hills",
       src: "images/hiroshige/Hiroshige_Evening_view_of_a_temple_in_the_hills.jpg",
-      color: "#748076",
-      darkerColor: "#454d47",
+      color: "#839987",
+      darkerColor: "#4C594F",
       text: " It is Hokusai's most famous work, and one of the best recognized works of Japanese art in the world. It depicts an enormous wave threatening boats off the coast of the prefecture of Kanagawa. While sometimes assumed to be a tsunami, the wave is, as the picture's title suggests, more likely to be a large rogue wave.[2] As in all the prints in the series, it depicts the area around Mount Fuji under particular conditions, and the mountain itself appears in the background."
     },
     { title: "Full moon over a mountain landscape",
@@ -20,32 +20,32 @@ $(function(){
     },
     { title: "Man on horseback crossing a bridge",
       src: "images/hiroshige/Hiroshige_Man_on_horseback_crossing_a_bridge.jpg",
-      color: "#748565",
-      darkerColor: "#47523e",
+      color: "#af9e72",
+      darkerColor: "#7d7151",
       text: " It is Hokusai's most famous work, and one of the best recognized works of Japanese art in the world. It depicts an enormous wave threatening boats off the coast of the prefecture of Kanagawa. While sometimes assumed to be a tsunami, the wave is, as the picture's title suggests, more likely to be a large rogue wave.[2] As in all the prints in the series, it depicts the area around Mount Fuji under particular conditions, and the mountain itself appears in the background."
     },
     { title: "People under maple trees by a stream",
       src: "images/hiroshige/Hiroshige_People_under_maple_trees_by_a_stream.jpg",
-      color: "#7f2918",
-      darkerColor: "#4d190e",
+      color: "#9F4533",
+      darkerColor: "#5F291F",
       text: " It is Hokusai's most famous work, and one of the best recognized works of Japanese art in the world. It depicts an enormous wave threatening boats off the coast of the prefecture of Kanagawa. While sometimes assumed to be a tsunami, the wave is, as the picture's title suggests, more likely to be a large rogue wave.[2] As in all the prints in the series, it depicts the area around Mount Fuji under particular conditions, and the mountain itself appears in the background."
     },
     { title: "Shrines in snowy mountains",
       src: "images/hiroshige/Hiroshige_Shrines_in_snowy_mountains.jpg",
-      color: "#304b80",
-      darkerColor: "#1d2d4d",
+      color: "#848283",
+      darkerColor: "#525051",
       text: " It is Hokusai's most famous work, and one of the best recognized works of Japanese art in the world. It depicts an enormous wave threatening boats off the coast of the prefecture of Kanagawa. While sometimes assumed to be a tsunami, the wave is, as the picture's title suggests, more likely to be a large rogue wave.[2] As in all the prints in the series, it depicts the area around Mount Fuji under particular conditions, and the mountain itself appears in the background."
     },
     { title: "Travellers surprised by sudden rain",
       src: "images/hiroshige/Hiroshige_Travellers_surprised_by_sudden_rain.jpg",
-      color: "#e9c24d",
-      darkerColor: "#b5973c",
+      color: "#E9BC35",
+      darkerColor: "#A98926",
       text: " It is Hokusai's most famous work, and one of the best recognized works of Japanese art in the world. It depicts an enormous wave threatening boats off the coast of the prefecture of Kanagawa. While sometimes assumed to be a tsunami, the wave is, as the picture's title suggests, more likely to be a large rogue wave.[2] As in all the prints in the series, it depicts the area around Mount Fuji under particular conditions, and the mountain itself appears in the background."
     },
     { title: "Evening snow at Kanbara",
       src: "images/hiroshige/Hiroshige_Evening_snow_at_Kanbara.jpg",
-      color: "#ee7d1d",
-      darkerColor: "#ba6217",
+      color: "#BEBEBE",
+      darkerColor: "#3E3E3E",
       text: " It is Hokusai's most famous work, and one of the best recognized works of Japanese art in the world. It depicts an enormous wave threatening boats off the coast of the prefecture of Kanagawa. While sometimes assumed to be a tsunami, the wave is, as the picture's title suggests, more likely to be a large rogue wave.[2] As in all the prints in the series, it depicts the area around Mount Fuji under particular conditions, and the mountain itself appears in the background."
     }
   ];
