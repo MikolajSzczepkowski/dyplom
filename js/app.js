ff63e47dc7302431c8ef6f6d56848c2d79e48a37
Mikolaj Szczepkowski <mikolajszczepkowski@gmail.com>
2016-05-25 10:39:00 +0200

    origami instructions

--- js/app.js
+++ js/app.js
@@ -57,135 +57,135 @@ $(function(){
       text: " It is Hokusai's most famous work, and one of the best recognized works of Japanese art in the world. It depicts an enormous wave threatening boats off the coast of the prefecture of Kanagawa. While sometimes assumed to be a tsunami, the wave is, as the picture's title suggests, more likely to be a large rogue wave.[2] As in all the prints in the series, it depicts the area around Mount Fuji under particular conditions, and the mountain itself appears in the background."
     }
   ],
-    svg = [
-      { path: "images/svg/crane-01.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-02.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-03.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-04.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-05.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-06.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-07.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-08.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-09.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-10.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-11.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-12.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-13.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-14.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-15.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-16.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-17.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-18.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-19.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-20.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-21.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-22.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-23.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-24.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-25.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-26.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-27.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-28.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-29.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-30.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-31.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-32.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-33.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-34.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-35.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-36.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-37.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-38.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-39.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-40.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-41.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      },
-      { path: "images/svg/crane-41.svg",
-        instruction: "Złóż kartkę przed sobą w żurawia."
-      }
-    ],
-    controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 500}});
+  svg = [
+    { path: "images/svg/crane-01.svg",
+      instruction: "Weź kwadratową kartkę i ułóż ją przed sobą."
+    },
+    { path: "images/svg/crane-02.svg",
+      instruction: "Złóż kartkę na pół w pionie."
+    },
+    { path: "images/svg/crane-03.svg",
+      instruction: "Wyprostuj kartkę."
+    },
+    { path: "images/svg/crane-04.svg",
+      instruction: "Złóż kartkę na poł w poziomie."
+    },
+    { path: "images/svg/crane-05.svg",
+      instruction: "Wyprostuj kartkę i odwróć."
+    },
+    { path: "images/svg/crane-06.svg",
+      instruction: "Złóż kartkę na pół po przekątnej."
+    },
+    { path: "images/svg/crane-07.svg",
+      instruction: "Wyprostuj kartkę."
+    },
+    { path: "images/svg/crane-08.svg",
+      instruction: "Złóż kartkę na pół po przeciwnej przekątnej."
+    },
+    { path: "images/svg/crane-09.svg",
+      instruction: "Wyprostuj kartkę."
+    },
+    { path: "images/svg/crane-10.svg",
+      instruction: "Obróć kartkę o 45 stopni."
+    },
+    { path: "images/svg/crane-11.svg",
+      instruction: "Chwyć dwa rogi po bokach i przyciągnij je do siebie. Górny róg powinien przykrywać całość."
+    },
+    { path: "images/svg/crane-12.svg",
+      instruction: "Zegnij jeden róg kwadratu aby jego krawędź pokryła się z przekątną."
+    },
+    { path: "images/svg/crane-13.svg",
+      instruction: "Powtórz z drugiej strony."
+    },
+    { path: "images/svg/crane-14.svg",
+      instruction: "Wyprostuj zgięcia."
+    },
+    { path: "images/svg/crane-15.svg",
+      instruction: "Chwyć za dolny róg i unieś, aby się otworzył."
+    },
+    { path: "images/svg/crane-16.svg",
+      instruction: "Zegnij kartkę do środka wzdłuż wcześniejszych zgięć."
+    },
+    { path: "images/svg/crane-17.svg",
+      instruction: "Zegnij kartkę do środka wzdłuż wcześniejszych zgięć po drugiej stronie."
+    },
+    { path: "images/svg/crane-18.svg",
+      instruction: "Popraw zgięcia spłaszczając kartkę."
+    },
+    { path: "images/svg/crane-19.svg",
+      instruction: "Podnieś i wyprostuj powstałą zakładkę."
+    },
+    { path: "images/svg/crane-20.svg",
+      instruction: "Zegnij zakładkę aby jej krawędź pokrywała się z przekątną."
+    },
+    { path: "images/svg/crane-21.svg",
+      instruction: "Zegnij zakładkę aby jej krawędź pokrywała się z przekątną z drugiej strony."
+    },
+    { path: "images/svg/crane-22.svg",
+      instruction: "Odwróć kartkę na drugą stronę i wykonaj te same kroki."
+    },
+    { path: "images/svg/crane-23.svg",
+      instruction: "Zegnij jeden róg kwadratu aby jego krawędź pokryła się z przekątną."
+    },
+    { path: "images/svg/crane-24.svg",
+      instruction: "Powtórz z drugiej strony."
+    },
+    { path: "images/svg/crane-25.svg",
+      instruction: "Wyprostuj zgięcia."
+    },
+    { path: "images/svg/crane-26.svg",
+      instruction: "Chwyć za dolny róg i unieś, aby się otworzył."
+    },
+    { path: "images/svg/crane-27.svg",
+      instruction: "Zegnij kartkę do środka wzdłuż wcześniejszych zgięć."
+    },
+    { path: "images/svg/crane-28.svg",
+      instruction: "Zegnij kartkę do środka wzdłuż wcześniejszych zgięć po drugiej stronie."
+    },
+    { path: "images/svg/crane-29.svg",
+      instruction: "Popraw zgięcia spłaszczając kartkę."
+    },
+    { path: "images/svg/crane-30.svg",
+      instruction: "Podnieś i wyprostuj powstałą zakładkę."
+    },
+    { path: "images/svg/crane-31.svg",
+      instruction: "Zegnij zakładkę aby jej krawędź pokrywała się z przekątną."
+    },
+    { path: "images/svg/crane-32.svg",
+      instruction: "Zegnij zakładkę aby jej krawędź pokrywała się z przekątną z drugiej strony."
+    },
+    { path: "images/svg/crane-33.svg",
+      instruction: "Chwyć za prawą krawędź i zgnij ją do lewej."
+    },
+    { path: "images/svg/crane-34.svg",
+      instruction: "Zegnij dolną część tak, aby jej wierzchołek pokrywał się z górnym."
+    },
+    { path: "images/svg/crane-35.svg",
+      instruction: "Zegnij górną wierzchnią część, aby powstała głowa żurawia."
+    },
+    { path: "images/svg/crane-36.svg",
+      instruction: "Chwyć za lewą krawędź i zegnij całoś na prawą stronę. Wymodeluj pod odpowiednim kątem szyję i głowę żurawia."
+    },
+    { path: "images/svg/crane-37.svg",
+      instruction: "Zegnij do środka lewą zakładkę, aby powstał ogon żurawia."
+    },
+    { path: "images/svg/crane-38.svg",
+      instruction: "Wymodeluj ogon żurawia pod odpowiednim kątem."
+    },
+    { path: "images/svg/crane-39.svg",
+      instruction: "Zegnij w dół wierzchnią część tak, aby powstało skrzydło żurawia."
+    },
+    { path: "images/svg/crane-40.svg",
+      instruction: "Powtórz z drugiej strony"
+    },
+    { path: "images/svg/crane-41.svg",
+      instruction: "Fantastycznie! Udało ci się złożyć żurawia."
+    },
+    { path: "images/svg/crane-41.svg",
+      instruction: "Fantastycznie! Udało ci się złożyć żurawia."
+    }
+  ],
+  controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 500}});
 
   $("#openAside").on("click", function(){
     if ($("#aside").hasClass("active")) {
